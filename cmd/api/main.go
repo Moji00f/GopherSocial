7781689ad2288c9d10838cd1d0b9a0f05bdbe495
Moji00f <mojtabaahmadi2008@gmail.com>
2025-03-15 22:25:17 +0330

    add dbConfig to api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,12 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
 	store := store.NewStorage(nil)

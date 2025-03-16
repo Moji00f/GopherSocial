70a3d7aeb72e9a156d75f3b2ba910ef2b9753ef6
Moji00f <mojtabaahmadi2008@gmail.com>
2025-03-16 14:55:13 +0330

    create getPostHandler for post handler

--- cmd/api/post.go
+++ cmd/api/post.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"github.com/Moji00f/GopherSocial/internal/store"
+	"github.com/go-chi/chi/v5"
 	"net/http"
+	"strconv"
 )
 
 type CreatePostPayload struct {
@@ -40,4 +43,30 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postId")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx := r.Context()
+
+	post, err := app.store.Posts.GetById(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 }
